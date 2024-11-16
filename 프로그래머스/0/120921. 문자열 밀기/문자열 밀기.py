364db0225cc8b5742df02ce0f2cc26fc4bd3dc90
Striver <96542456+T3Tm@users.noreply.github.com>
2024-11-16 15:15:11 +0900

    [level 0] Title: 문자열 밀기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,8 +1,4 @@
 def solution(A, B):
-    A=A+A
-    if B not in A:return -1
-    #hellohello
-    #atatatat
-    for i in range(len(A)-len(B),0, -1):
-        if A[i:i + len(B)] == B:
-            return len(B) - i 
+    B=B+B
+    if A not in B:return -1
+    return B.index(A)
