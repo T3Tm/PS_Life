1f2301a015390d988d0d6216d679d4dc95757dbf
Striver <96542456+T3Tm@users.noreply.github.com>
2025-08-05 16:10:50 +0900

    [level 2] Title: 올바른 괄호, Time: 6.68 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,23 +1,27 @@
-# def solution(s):
-#     a=[]
-#     for i in s:
-#         if i=='(':a.append(i)
-#         else:
-#             if len(a):
-#                 if a.pop()!='(':
-#                     return False
-#             else:return False
-#     if len(a):return False
-#     return True
+'''
+'25. 08. 05
+1. 올바른 괄호의 쌍을 알아내어라.
+2. () 이런 것이 올바른 괄호
+3. )( 이런 것은 올바른 괄호가 아니다.
 
+
+1. 현재 보고 있는 문자가 단순히 ( 라면 스택에 넣는다.
+2. )라면 스택에 ( 문자가 있는지 확인한다.
+3. 없다면 해당 괄호는 성립할 수 없으므로 false 리턴한다.
+4. 모든 문자를 다 마친 뒤 스택에 괄호가 남아있다면 이 문자열도 성립할 수 없는 괄호이다.
+'''
 def solution(s):
-    Stack = []
-    jjak = {'(' : ')', ')' : '('}
-    for pal in s:
-        if pal == '(':Stack.append(pal)
+    answer = True
+    stack = []
+    
+    for word in s:
+        if word == '(':stack.append(0)
+        #스택 안에 있는지 확인
         else:
-            if not Stack:return False
-            if jjak[Stack[-1]] != pal:return False
-            Stack.pop()
-    if Stack:return False
-    return True
+            if stack:stack.pop()
+            #없는 경우
+            else:
+                answer = False
+                break
+    if stack: answer = False
+    return answer
