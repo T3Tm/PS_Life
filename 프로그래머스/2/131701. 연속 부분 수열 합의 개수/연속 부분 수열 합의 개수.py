cd8542c21996e6b98e2dd3a589a3e851a51cf611
Striver <96542456+T3Tm@users.noreply.github.com>
2025-08-24 00:55:20 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 294.66 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,26 +1,28 @@
-from collections import deque
+'''
+'25. 8. 24
+
+1. 연속 부분 수열의 합
+2. 길이가 최대가 1000이기 때문에
+
+스위핑 기법을 이용하면 쉽게 구할 수 있을 것이다.
+'''
 def solution(elements):
-    comb = set()
-    #부분 수열의 합으로 만들 수 있는 가짓 수
-    #연속 부분 수열 합 << 슬라이딩 윈도우
+    len_ = len(elements)
+    elements = elements + elements
+
+    save_number = set()#이전 숫자들 기억
     
-    for size in range(1, len(elements) + 1):#크기
-        #전처리
-        data = deque()
-        hap = 0
-        for i in range(size):
-            data.append(elements[i])
-            hap += elements[i]
-        
-        comb.add(hap)
-        
-        for idx in range(size, len(elements)+size-1):
-            pre = data.popleft()
-            hap -= pre
-            
-            nex = elements[idx%len(elements)]
-            data.append(nex)
-            hap += nex
-            
-            comb.add(hap)
-    return len(comb)
+    for length in range(1, len_ + 1):
+        front, back = 0, 0
+        sum_ = 0
+        for p in range(length):
+            sum_ += elements[back]
+            back += 1
+
+        save_number.add(sum_)#현재 값도 저장
+        while front < len_:
+            sum_ = sum_ - elements[front] + elements[back]
+            front += 1
+            back += 1
+            save_number.add(sum_)
+    return len(save_number)
