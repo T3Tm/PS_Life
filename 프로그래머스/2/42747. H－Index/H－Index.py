51635db335bdcdf7f7483625a1b0fb03f6131ac7
Striver <96542456+T3Tm@users.noreply.github.com>
2025-08-31 12:49:24 +0900

    [level 2] Title: H-Index, Time: 5.86 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,16 +1,22 @@
-from bisect import bisect_left
+'''
+'25. 08. 31
+
+1. H-index 는 n편 중 h번 이상 인용된 논문이 h편 이상이고, 나머지 논문은 h번 이하 인용되었다면, h의 최댓값이 h-index이다.
+
+0, 1, 3, 5, 6
+정렬한 뒤 해당 숫자와 값을 통해서 몇 편인지 알 수 있다.
+
+0 0 0 0 0
+'''
+from bisect import bisect_left as bl
 def solution(citations):
-    answer = 0
+    answer = -1
+    citations.sort()#최소와 최대
     
-    citations.sort()
-    print(citations)
     size = len(citations)
-    
-    for h in range(max(citations) + 1):
-        idx = bisect_left(citations, h)
+    for i in range(10001):
+        idx = bl(citations, i)
         
-        better_h_count = size - idx
-        small_h_count = idx + 1 - (citations[idx] != h) 
-        if better_h_count >= h and small_h_count <= h:
-            answer = max(answer, h)
+        if i <= size - idx and idx <= i:
+            answer = max(i, answer)
     return answer
