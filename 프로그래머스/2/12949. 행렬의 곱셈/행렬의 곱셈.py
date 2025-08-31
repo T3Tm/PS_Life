e6962031e8fd4718892afae77ccd8f8bc1272c50
Striver <96542456+T3Tm@users.noreply.github.com>
2025-08-31 12:31:20 +0900

    [level 2] Title: 행렬의 곱셈, Time: 63.29 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,19 +1,25 @@
+'''
+'25. 08. 31
+
+1. arr1, arr2의 곱을 반환하는 함수를 완성하여라.
+
+arr1 3, 2
+arr2 2, 2
+
+rst 3, 2
+
+rst[i][j] := sum(arr1[i][:] * arr2[:][j])이다.
+'''
 def solution(arr1, arr2):
-    answer = []
-    n = len(arr1)
-    m = len(arr2[0])
+    new_row = len(arr1)
+    new_col = len(arr2[0])
+    answer = [[0]*new_col for _ in range(new_row)]
     
-    repeat = len(arr1[0])
-    
-    for i in range(n):
-        tmp = []
-        for j in range(m):
-            num = 0
-            
-            for col in range(repeat):
-                
-                num += arr1[i][col] * arr2[col][j]
-            
-            tmp.append(num)
-        answer.append(tmp)
+    #10^4
+    for i in range(new_row):
+        for j in range(new_col):
+            v = 0
+            for col in range(len(arr1[0])):
+                    v += arr1[i][col] * arr2[col][j]
+            answer[i][j] = v
     return answer
