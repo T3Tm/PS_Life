a4d10af940d81a39a13025df4fe6e050d482ed1b
Striver <96542456+T3Tm@users.noreply.github.com>
2025-09-04 21:42:15 +0900

    [level 2] Title: [1차] 캐시, Time: 126.72 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,20 +1,34 @@
+'''
+'25. 09. 04
 
+1. LRU 알고리즘.
+2. hit일 때는 1, miss 5
+
+
+5, 10, 15, 20, 25, 30, 35, 40, 45, 50
+
+5, 10, 15, 16, 17, 18, 19, 20, 21
+
+5, 10, 15, 20, 25, 30, 35, 40, 45, 50,
+'''
 def solution(cacheSize, cities):
-    if cacheSize == 0: return len(cities) * 5
-    dic = {}
-    cnt = 0
-    for i in range(len(cities)):
-        t=cities[i].upper()
-        if t in dic:
-            dic[t] = i
-            cnt += 1
-        else:
-            if len(dic) + 1 <= cacheSize:
-                dic[t] = i
-                cnt += 5
-            else:
-                item = sorted(dic.items(), key=lambda x: -x[1])
-                del dic[item[-1][0]]
-                dic[t] = i
-                cnt += 5
-    return cnt
+    lru = {}
+    
+    answer = 0
+    for idx, value in enumerate(cities):
+        value = value.lower()
+        if value in lru:#cacheHit
+            answer += 1
+        else:#cacheMiss
+            answer += 5
+            #가장 오래된 idx 삭제
+        lru[value] = idx
+        
+        if len(lru) > cacheSize:
+            target_key, target_value = '', 0x3f3f3f3f
+            for key, data in lru.items():
+                if data < target_value:
+                    target_value = data
+                    target_key = key
+            del lru[target_key]
+    return answer
