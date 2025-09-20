28c6a9c00a0e2d4cfb3ac396403126d2a24dc47f
Striver <96542456+T3Tm@users.noreply.github.com>
2025-09-20 16:57:23 +0900

    [level 2] Title: 전화번호 목록, Time: 430.57 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -10,30 +10,14 @@
 def solution(phone_book):
     answer = True
     
-    phone_book.sort(key = lambda x: (x, len(x)))
-    
-    #print(phone_book)
-    #해시로 만든다?
-    
-    #접두어로 있다..
-    #118
-    #1194
-    #11946
-    #119478 이런 케이스가 있을 수도 있다.
-    #11821787823 << 이런케이스를 빠르게 구할 수있으려면
-    
+    hash_table = set()
+    for phone_number in phone_book:
+        hash_table.add(phone_number)
     
     for idx, phone_number in enumerate(phone_book):
-        for next_idx in range(idx + 1, len(phone_book)):
-            if phone_number not in phone_book[next_idx]:break
-            #첫 글자가 다르면 더이상 볼 필요가 없음
-            A_idx = B_idx = 0
-            while A_idx < len(phone_number) and B_idx < len(phone_book[next_idx]) and \
-                phone_number[A_idx] == phone_book[next_idx][B_idx]:
-                A_idx += 1
-                B_idx += 1
-            
-            if A_idx == len(phone_number):
+        tmp = ''
+        for word in phone_number:
+            tmp += word
+            if tmp in hash_table and len(tmp) != len(phone_number):
                 answer = False
-        
     return answer
