365a95580c2e275a57dba08be0677264ad6517f6
Striver <96542456+T3Tm@users.noreply.github.com>
2025-09-21 16:58:50 +0900

    [level 2] Title: 프로세스, Time: 0.38 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,39 +1,36 @@
-# from collections import deque
-# def solution(priorities, location):
-#     answer = 0
-#     idx_q = deque([i for i in range(len(priorities))])
-#     q = deque(priorities)
-#     max_value = max(q)
-#     while q:
-#         if(q[0] == max_value):
-#             answer+=1
-#             q.popleft()
-#             if(idx_q.popleft() == location):
-#                 break
-#             max_value = max(q)
-#             continue
-#         q.rotate(-1)
-#         idx_q.rotate(-1)
-#     return answer
+'''
+'25. 09. 21.(일)
+
+1. 프로세스 하나 꺼내기(젤 앞에 있는 것)
+2. 우선순위가 더 높은 것이 있다면 다시 넣기
+3. 없으면 현재 1번에서 뽑은 것 실행
+
+1 <= xi <= 100 (배열의 길이)
+
+location의 위치가 몇 번째로 실행되는가
+'''
 from collections import deque
 def solution(priorities, location):
-    #1. 프로세스 하나 꺼내기
-    #2. 우선순위가 가장 큰 프로세스 꺼내기
-    #3. 
-    deq = deque()
-    for idx, value in enumerate(priorities):
-        deq.append((idx, value))
-    answer = 1
-    while deq:
-        max_q = -1
-        for value in deq:
-            max_q = max(max_q, value[1])
-            
-        front = deq.popleft()
-        if front[1] != max_q:#최댓값 아님
-            deq.append(front)
+    answer = 0
+    dq = deque([[value, i] for i, value in enumerate(priorities)])#queue로 만들어주기
+    
+    while 1:
+        value, Index = dq.popleft()
+        #나머지 벨류 값 중에서 높은 것이 없는지 확인
+        is_better = False
+        for v, idx in dq:
+            if value < v:
+                is_better = True
+                break
+        #큰 것이 있음
+        if is_better:
+            dq.append([value, Index])
             continue
+        #여기까지 왔다면 현재 수를 하나는 뺀 것임.
         
-        if front[0] == location:break
         answer += 1
+        #없으면 현재 값을 없앨 것임.
+        #내가 찾던 숫자인지 확인
+        if Index == location:
+            break
     return answer
